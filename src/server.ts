c4e6a39cd6e50ef8ad9f36a88505abe839b4dd74
vishnu-a-b <vishnuab1207@gmail.com>
2024-10-05 12:33:42 +0530

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import app from "./app"
+const port=3001
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
